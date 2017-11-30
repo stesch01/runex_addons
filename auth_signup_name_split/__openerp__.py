9d30d013e0325b822dea43c2e0d46f540daa5377
gediminasv <gediminasv@live.com>
2017-11-30 12:34:52 +0200

    [IMP]manifest change

--- auth_signup_name_split/__openerp__.py
+++ auth_signup_name_split/__openerp__.py
@@ -1,26 +1,38 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
 {
-    'name': "auth_signup_name_split",
-
-    'summary': """
-        auth_signup_name_split""",
-
+    "name": "Authentication Signup Name Split",
+    "version": "1.0",
+    "author": "Linserv AB",
+    "category": "Authentication",
+    "summary": "Authentication Signup Name Split Module",
+    "website": "www.linserv.se",
+    "contributors": [
+        'Gediminas Venclova <gediminasv@live.com>'
+    ],
+    "license": "",
+    "depends": [
+        'base', 'auth_signup'
+    ],
     'description': """
-        auth_signup_name_split
+
+        Authentication Signup Name Split Module
+
+        This module splits name field into name and surname in signup screen.
     """,
-
-    'author': "UAB Pralo",
-    'website': "http://www.pralo.eu",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['base', 'auth_signup'],
-
-    'data': [
+    "demo": [],
+    "data": [
         'auth_signup_login_inherited.xml',
     ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
 }
