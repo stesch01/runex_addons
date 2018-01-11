29fd32d8bdbfcf927bc80f08dbe22c64417caadc
Riyaj Pathan <rjpathan19@gmail.com>
2018-01-12 04:26:09 +0530

    module to make company registry field as stored

---
+++ partner_company_registry/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+{
+    'name': 'Parnter - Company Registry',
+    'version': '1.0',
+    'author': 'Linserv AB',
+    'category': "Base",
+    'website': 'www.linserv.se',
+    'description': """Parnter - Company Registry""",
+    'depends': ['l10n_se'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
