218813e7fa7de763ab2cd5e1f5ddfde7f92d4ca8
Riyaj Pathan <rjpathan19@gmail.com>
2018-01-18 07:27:00 +0530

    change function field computation to old v7 api

--- sale_order_line_view/models/res_partner.py
+++ sale_order_line_view/models/res_partner.py
@@ -2,19 +2,23 @@
 # (c) 2015 Esther Martín <esthermartin@avanzosc.es> - Avanzosc S.L.
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from openerp.osv import fields,osv
 
-
-class ResPartner(models.Model):
+class ResPartner(osv.osv):
     _inherit = 'res.partner'
 
-    @api.multi
-    @api.depends('order_lines')
-    def _sale_order_line_count(self):
-        for partner in self:
-            partner.lines_count = len(partner.order_lines)
+    def _sale_order_line_count(self, cr, uid, ids, field_name, arg, context=None):
+        res = dict(map(lambda x: (x,0), ids))
+        # The current user may not have access rights for sale orders
+        try:
+            for partner in self.browse(cr, uid, ids, context):
+                res[partner.id] = len(partner.order_lines)
+        except:
+            pass
+        return res
 
-    order_lines = fields.One2many('sale.order.line', 'order_partner_id',
-                                  'Sale order lines')
-    lines_count = fields.Integer('Sale order lines',
-                                 compute='_sale_order_line_count', store=True)
+    _columns = {
+        'lines_count': fields.function(_sale_order_line_count, string='Sale Order Lines', type='integer'),
+        'order_lines': fields.one2many('sale.order.line','order_partner_id','Sales Order lines')
+    }
+    
