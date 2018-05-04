8df9ece741a1c96dbef38f419fb25286daa0c2ee
orej <j.orechovas@pralo.eu>
2018-05-04 12:22:19 +0300

    [ADD] Package of Runex modifications in app

---
+++ runex_webshop_2_0/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
+{
+ "name": "RUNEX webshop 2.0",
+ "version": "1.0",
+ "author": "Linserv AB",
+ "category": "Website",
+ "summary": "RUNEX webshop 2.0",
+ "website": "www.linserv.se",
+ "contributors": [
+      'Gediminas Venclova <gediminasv@live.com>'
+ ],
+ "license": "",
+ "depends": ['website', 'website_sale'],
+ 'description': """
+
+        Package of Runex Webshop modifications
+
+    """,
+ "demo": [],
+ "data": ['views/website_sale_template_inherited.xml'],
+ "test": [],
+ "js": [],
+ "css": [],
+ "installable": True,
+ "auto_install": False,
+}
