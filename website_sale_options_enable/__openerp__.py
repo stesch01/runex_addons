18cfb2f6a989643025226eb401a5a08aa287bf65
Az Ad <ezer1989@gmail.com>
2018-05-15 14:56:05 +0100

    Popup when cart button is clicked in product list

---
+++ website_sale_options_enable/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'eCommerce Optional Products Enable',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Linserv AB',
+    'sequence': 1,
+    'summary': 'Skip payment for public users',
+    'website': 'www.linserv.se',
+    'contributors': [
+        'Azer GHADHOUN <ghadhoun.azer@gmail.com>'
+    ],
+    'description': """
+        """,
+    'depends': ['website_sale_options'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
