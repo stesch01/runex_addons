7b67b89c3bf3202a25fe67c2cb6dc83e6fad4452
Riyaj Pathan <rjpathan19@gmail.com>
2018-05-16 01:25:14 +0530

    make Discount field readonly if Partner is Contact and set Discount to that of his Parent company, if set

--- so_header_discount/models/partner.py
+++ so_header_discount/models/partner.py
@@ -14,3 +14,40 @@ class ResPartner(models.Model):
 
     so_discount = fields.Float('Discount %', digits_compute=dp.get_precision('Account'))
     
+    @api.onchange('is_company', 'parent_id')
+    def onchange_is_company(self):
+        for partner in self:
+            if not partner.is_company:
+                partner.so_discount = partner.parent_id and partner.parent_id.so_discount or 0
+
+
+    @api.model
+    def create(self, vals):
+        if not vals: vals = {}
+        if 'is_company' in vals:
+            if vals['is_company'] is False:
+                if vals['parent_id']:
+                    company_discount = self.browse([vals['parent_id']]).so_discount
+                    vals['so_discount'] = company_discount
+        return super(ResPartner, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if not vals: vals = {}
+        for partner in self:
+            if 'is_company' in vals or 'parent_id' in vals:
+                is_company, parent = False, False
+                if 'is_company' in vals:
+                    is_company = vals['is_company']
+                else:
+                    is_company = partner.is_company
+
+                if is_company is False:
+                    if 'parent_id' in vals:
+                        parent = vals['parent_id']
+                    else:
+                        parent = partner.parent_id.id
+
+                    company_discount = self.browse([parent]).so_discount
+                    vals['so_discount'] = company_discount
+            return super(ResPartner, partner).write(vals)
