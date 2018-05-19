9785243034a1951014fac37054d27818f071901e
Az Ad <ezer1989@gmail.com>
2018-05-19 16:53:47 +0100

    Found a best way to highlight input text

--- website_sale_search_mark/static/src/js/website_sale.js
+++ website_sale_search_mark/static/src/js/website_sale.js
@@ -3,110 +3,74 @@ $(document).ready(function () {
     if(! $("input.search-query").length){
         return;
     }
-    function isCharacterKeyPress(evt) {
-        if (typeof evt.which == "undefined") {
-            // This is IE, which only fires keypress events for printable keys
-            return true;
-        } else if (typeof evt.which == "number" && evt.which > 0) {
-            // In other browsers except old versions of WebKit, evt.which is
-            // only greater than zero if the keypress is a printable key.
-            // We need to filter out backspace and ctrl/alt/meta key combinations
-            return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
-        }
-        return false;
-    }
-    function CharOffset(el) {
-        var caretOffset = 0;
-        if (typeof window.getSelection != "undefined") {
-            var range = window.getSelection().getRangeAt(0);
-            var preCaretRange = range.cloneRange();
-            preCaretRange.selectNodeContents(el);
-            preCaretRange.setEnd(range.endContainer, range.endOffset);
-            caretOffset = preCaretRange.toString().length;
-        } else if (typeof document.selection != "undefined" && document.selection.type != "Control") {
-            var textRange = document.selection.createRange();
-            var preCaretTextRange = document.body.createTextRange();
-            preCaretTextRange.moveToelText(el);
-            preCaretTextRange.setEndPoint("EndToEnd", textRange);
-            caretOffset = preCaretTextRange.text.length;
-        }
-        return caretOffset;
-    }
-    function setCursor(node,pos){
-        if(!node){
-            return false;
-        } else if(document.createRange) {
-          range = document.createRange();
-          range.selectNodeContents(node);
-          range.setStart(node, pos);
-          range.setEnd(node, pos);
-          selection = window.getSelection();
-          selection.removeAllRanges();
-          selection.addRange(range);
-        } else if(node.createTextRange) {
-            var textRange = node.createTextRange();
-            textRange.collapse(true);
-            textRange.moveEnd(pos);
-            textRange.moveStart(pos);
-            textRange.select();
-            return true;
-        } else if(node.setSelectionRange) {
-            node.setSelectionRange(pos,pos);
-            return true;
-        }
-        return false;
-    }
-    var $search = $('input.search-query');
-    var search_width = $search[0].getBoundingClientRect().width;
-    $editable = $('<div/>')
+    // To make the words highlighted
+    // Solution in a nutshell:
+    // * Wrap the input and add a div "highlighted"  next to it
+    //   with contenteditable activated to mimic its behaiviour
+    // * Hide the input
+    // * Add a new div "highlight" with same paddings , margin
+    //   and border as the added "highlighted" editable div.
+    // * The "highlight" copies values from "highlighted" div
+    //   and wraps the text that should be marked in spans
+    //   with a background and a transparent color for text.
+    // * The text from "highlight" and "highlighted" are superposed
+    //   --> "highlight" div have a transparent text color
+    //       and "highlighted" div have transparent background.
+    var $input = $('input.search-query');
+    var width = $input.outerWidth();
+    var height = $input.outerHeight();
+    var $highlight_wrap = $('<div/>').addClass('highlight_wrap');
+    var $highlight = $('<div/>').addClass('highlight');
+    var $editable = $('<div/>')
+        .addClass('highlighted')
         .attr('contenteditable', 'true')
-        .attr('placeholder', $search.attr('placeholder'))
-        .addClass($search.attr('class'))
-        .addClass('highlighted single-line')
-        .css({
-            'width':search_width,
-        })
-        .text($search.val());
-    $search.after($editable).hide();
-    function _update_marks($search, $editable, noRange){
-        var editable = $editable.get(0);
-        $search.val($editable.text());
-        var divText = $editable.text().split('');
-        var pattern = /(\d|\w)/;
-        var replaceWith = '<span class="highlight">$1</span>';
-        var highlighted = divText.map(function(char) {
-          if (pattern.test(char))
-              return $(char.replace(pattern, replaceWith));
-          else
-              return document.createTextNode(char);
-        });
-        if (noRange){
-            $editable.empty().append(highlighted);
+        .attr('placeholder', $input.attr('placeholder'));
+    $input.wrap($highlight_wrap);
+    $input.before($highlight);
+    $input.before($editable);
+    $input.before($editable).hide();
+    $highlight_wrap = $input.closest('.highlight_wrap');
+    $highlight_wrap.css('width', width);
+    $highlight_wrap.css('height', height);
+    $highlight_wrap.addClass($input.attr('class'));
+    $editable.text($input.val());
+    var pattern = /(\S+)/g;
+    var _scrolled_search = function(pattern, $input, $editable, $highlight){
+        // regexp that match anything exept whitespaces
+        var text = $editable.text();
+        // replace &nbsp; with spaces to fix non valid submission
+        $input.val(text.replace(/\u00a0/g, " "));
+        if (!pattern.test(text)) {
+            // Make it faster if no regex matched
+            $highlight.text(text);
             return;
         }
-        var Position = CharOffset(editable);
-        $editable.empty().append(highlighted);
-        setCursor(editable, Position);
-    }
-    _update_marks($search, $editable, true);
+        var replaceWith = '<span class="marker">$1</span>';
+        $highlight.html(text.replace(pattern, replaceWith));
+    };
+    _scrolled_search(pattern, $input, $editable, $highlight);
     $editable.on('keyup', function(e){
-        // space (keycode 32) fix for firefox
-        if(navigator.userAgent.indexOf("Firefox") != -1){
-            if (e.which == 32) return;
-        }
-        // Mimmic input enter click: submit on enter clicked
-        if (e.which == 13) $editable.closest('form').submit();
-        _update_marks($search,$editable);
+        _scrolled_search(pattern, $input, $editable, $highlight);
     });
-    // copy text in case user typped in searchbox before it got hidden
-    $search.on('keyup', function(e){
-        $editable.text($search.val());
+    // In case of user enters text before search input is hidden
+    $input.on('input', function(){
+        $editable.text($input.val());
     });
-//    FIX to show placeholder on div
+    //    FIX to show placeholder on div
     $editable.on('focusout keyup', function(){
         var element = $(this);
         if (!element.text().trim().length) {
             element.empty();
         }
     });
+    $editable.on('keypress', function(e){
+        // if enter is pressed , just submit
+        // then cancel the keypress (no newline, we do not need it)
+        if(e.keyCode== 13 || e.which== 13) { //if enter key is pressed
+            $editable.closest('form').submit();
+            // cancel the keypress
+            return false;
+        }
+    });
+
 });
