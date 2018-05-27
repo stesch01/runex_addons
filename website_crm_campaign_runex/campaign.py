e74b544febb3ef5be48f267626cbf2344a61f1d6
Az Ad <ezer1989@gmail.com>
2018-05-27 13:41:04 +0100

    custom the filter search

--- website_crm_campaign_runex/campaign.py
+++ website_crm_campaign_runex/campaign.py
@@ -490,3 +490,31 @@ class website_sale(website_sale):
             return request.env['ir.translation'].search([('res_id', '=', product.id), ('name', '=', 'product.template,name'), ('type', '=', 'model'), ('lang', '=', context.get('lang'))])[-1].value
         except:
             return product.name
+
+    def _get_search_domain(self, search, category, attrib_values):
+        domain = request.website.sale_product_domain()
+
+        if search:
+            search_fields = request.env['ir.config_parameter'].get_param('alt.product.search.fields', 'name description description_sale product_variant_ids.default_code').split(" ")
+            for srch in search.split(" "):
+                domain += ['|' for x in range(len(search_fields) - 1)] + [(f, 'ilike', srch) for f in search_fields]
+        if category:
+            domain += [('public_categ_ids', 'child_of', int(category))]
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+
+        return domain
